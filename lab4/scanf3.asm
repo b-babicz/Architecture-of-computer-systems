6dd387defb7fe9d39a12d69ed7eabee8b25da5fd
Bartłomiej Babicz <102216334+b-babicz@users.noreply.github.com>
2023-05-20 18:38:11 +0200

    Add files via upload

---
+++ lab4/scanf3.asm
@@ -0,0 +1,57 @@
+         [bits 32]
+
+extern   _printf
+extern   _scanf
+extern   _exit
+
+a        equ 1
+
+global   _main
+
+section  .text
+
+section  .a
+         db a
+
+_main:
+
+;        esp -> [ret]  ; return address
+
+         push format  ; *(int*)(esp-4) = format ; esp = esp - 4
+
+;        esp -> [format][ret]
+
+         call _printf  ; printf('a = ');
+         
+;        esp -> [a][ret]  ; zmienna a, adres format nie jest juz potrzebny
+
+         push esp  ; esp -> stack
+         
+;        esp -> [addr_a][a][ret]
+
+         push format2  ; *(int*)(esp-12) = format2 ; esp = esp - 12
+         
+;        esp -> [format2][addr_a][a][ret]
+
+         call _scanf  ; scanf(format2, addr_a)
+
+         add esp, 2*4  ; esp = esp + 8
+
+;        esp -> [a][ret]
+
+         push format3
+         
+;        esp -> [format3][a][ret]
+
+         call _printf  ; printf("a = %d\n", a);
+         add esp, 2*4  ; esp = esp + 8
+         
+;        esp -> [ret]
+
+         push 0      ; esp -> [0][ret]
+         call _exit  ; exit(0);
+         
+format   db 'a = ', 0
+format2  db '%d', 0
+format3  db 'a = %d', 0xA, 0
+
